76cd51a24af3ac8966df1414bf368a9fdec54244
KarthikThakkolu <150405840+KarthikThakkolu@users.noreply.github.com>
2024-01-05 14:43:27 +0530

    hii

--- src/components/Pharmacy/PharmacyBilling.jsx
+++ src/components/Pharmacy/PharmacyBilling.jsx
@@ -171,6 +171,7 @@ const PharmacyBilling = () => {
         setmobilenumber('');
         setdoctorName('');
         setTaxableValues('');
+        setSelectedProduct('');
         setSign('');
         setDate('');
         setItems([
